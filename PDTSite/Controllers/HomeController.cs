0dace1d0879c2b206fce2bfcd01aa993ca873a0c
Mayank Katwal <Mayank Katwal>
2016-08-01 21:55:51 -0500

    Initial Commit

---
+++ PDTSite/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PDTSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "UW-Madison Phi Delta Theta Alpha Chapter";
+            return View();
+        }
+        public ActionResult MeetTheBrothers()
+        {
+            ViewBag.Message = "This is where we will put the information about the brothers.";
+
+            return View();
+        }
+        public ActionResult JoinUs()
+        {
+            ViewBag.Message = "This is where we will put the information about the brothers.";
+
+            return View();
+        }
+        public ActionResult Teaser()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PDTSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PDTSite.Startup))]
+namespace PDTSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
